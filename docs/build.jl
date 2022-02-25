0b65193c0138ad5ccc3d8bfdc5e6d39a706770bf
Rik Huijzer <rikhuijzer@pm.me>
2022-02-25 14:15:42 +0100

    Add GLM demo (#1)

---
+++ docs/build.jl
@@ -0,0 +1,24 @@
+const NOTEBOOKS_DIR = joinpath(pkgdir(JModels), "docs", "src", "notebooks")
+
+"""Run all Pluto notebooks (".jl" files) in `NOTEBOOKS_DIR` and write outputs to files."""
+function build()
+    println("Building notebooks")
+    # Evaluate notebooks in the same process to avoid having to recompile from scratch each time.
+    # This is similar to how Documenter and Franklin evaluate code.
+    # Note that things like method overrides and other global changes may leak between notebooks!
+    use_distributed = false
+    output_format = documenter_output
+    bopts = BuildOptions(NOTEBOOKS_DIR; use_distributed, output_format)
+    parallel_build(bopts)
+    return nothing
+end
+
+"Return Markdown file links which can be passed to Documenter.jl."
+function markdown_files()
+    md_files = map(notebooks) do tutorial
+        file = lowercase(replace(tutorial, " " => '_'))
+        return joinpath("notebooks", "$file.md")
+    end
+    return md_files
+end
+
